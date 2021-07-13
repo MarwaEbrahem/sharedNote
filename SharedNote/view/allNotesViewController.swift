1dc706b6c2bfda5b77bba5841b3309a8c498fb9b
MarwaEbrahem <marwa15shanab@gmail.com>
2021-07-12 21:59:06 -0500

    read from firebase

--- SharedNote/view/allNotesViewController.swift
+++ SharedNote/view/allNotesViewController.swift
@@ -9,18 +9,29 @@
 import UIKit
 import RxCocoa
 import RxSwift
+import FirebaseDatabase
 class allNotesViewController: UIViewController {
     @IBOutlet weak var allNotesTableView: UITableView!
     var disposeBag = DisposeBag()
+     private let database = Database.database().reference()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         allNotesTableView.delegate = self
        Observable.just(["marwa","rovan"]).bind(to: allNotesTableView.rx.items(cellIdentifier: Constants.noteTableCell)){row,item,cell in
         (cell as? noteTableViewCell)?.noteLabel.text = item
         (cell as? noteTableViewCell)?.noteDelegate = self
         }.disposed(by: disposeBag)
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        database.child("note").observe(.value) { (DataSnapshot) in
+            guard let value = DataSnapshot.value  else {
+                print("error")
+                return
+            }
+            print(value)
+        }
+    }
     @IBAction func addNoteBtn(_ sender: Any) {
         let addNoteViewController = storyboard?.instantiateViewController(identifier: Constants.addNote) as! addNoteViewController
         navigationController?.pushViewController(addNoteViewController, animated: true)
